149c85a036afb21a7d4218c0b0e13800bb38bf8a
Massimiliano D'Amico <max@MBPdiMaimiliano.station>
2019-09-25 00:31:28 +0200

    update

--- anomaly_detection/anomalyDetectionAutoencoder.py
+++ anomaly_detection/anomalyDetectionAutoencoder.py
@@ -1,5 +1,6 @@
 import json
 import numpy as np
+import os
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.externals import joblib 
@@ -11,109 +12,71 @@ from keras.optimizers import Adam
 from keras.callbacks import TensorBoard, EarlyStopping
 from keras.losses import mse, binary_crossentropy
 from keras import backend as K
-import dataLayer as dl
 import pandas as pd
 import seaborn as sn
 import matplotlib.pyplot as plt
+import random
+from sklearn.utils import shuffle
 #%matplotlib inline
 
-#set del seed per la randomizzazione
-np.random.seed(1671)
+batch_size=32
+epochs=1
+lr=0.001
 
-#Autoencoder
-class Autoencoder(object):
+#autoencoder
+input_dim = 4
+hidden_dim = 3
+encoding_dim = 2
+dropout = 0.3
+inputLayer = Input(shape=(input_dim,))
+encoderLayer = Dense(hidden_dim, activation="relu")(inputLayer)
+#encoderLayer = Dropout(dropout)(encoderLayer)
+encoderLayer = Dense(encoding_dim, activation="relu")(encoderLayer)
+decoderLayer = Dense(hidden_dim, activation='relu')(encoderLayer)
+#decoderLayer = Dropout(dropout)(decoderLayer)
+decoderLayer = Dense(input_dim, activation='sigmoid')(decoderLayer)
+model = Model(inputs=inputLayer, outputs=decoderLayer)
+optimizer = Adam(lr=lr)
+model.compile(optimizer=optimizer, loss='mean_squared_error', metrics=['accuracy'])
 
-    def __init__(self):
-        self.autoencoderModelFile = 'tmp/ae.json'
-        self.autoencoderWeightFile= 'tmp/ae.h5'
-        self.verbose=1
+'''
+history = model.fit(X, X, epochs=epochs, batch_size=batch_size, shuffle=True, validation_split=0.3, verbose=1)
+score = model.evaluate(X, X, verbose=0)
+print(score)
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+'''
 
-        #hyperparameters
-        self.use_dropout = True
-        self.use_autodimension = True
-        self.encoding_dim = 32
-        #self.encoding_dim = 16
-        self.hidden_dim = self.encoding_dim * 2
-        self.dropout = 0.3
-        self.batch_size=32
-        self.epochs=200
-        self.shuffle=False
-        self.validation_split=0.3
-        self.lr=0.001
-        
-    def buildAndFit(self, X):
-        #log
-        print("ADDESTRAMENTO AUTOENCODER")
+def generator(data, batch_size):
+    counter=0
+    while True:
+        batch = np.array(data[batch_size*counter:batch_size*(counter+1)]).astype('float32')
+        counter += 1
+        if(len(batch) > 0): counter = 0
+        yield batch, batch
 
-        #attrubuisce le dimensioni degli strati in automatico in base alla dimensione dell'input
-        if self.use_autodimension:
-            self.encoding_dim = int(X.shape[1] / 4)
-            self.hidden_dim = int(X.shape[1] / 2)
-
-        #crea il modello autoencoder
-        in_out_neurons_number = X.shape[1]
-        inputLayer = Input(shape=(in_out_neurons_number, ))
-        encoderLayer = Dense(self.hidden_dim, activation="tanh")(inputLayer)
-        if self.use_dropout:
-            encoderLayer = Dropout(self.dropout)(encoderLayer)
-        encoderLayer = Dense(self.encoding_dim, activation="tanh")(encoderLayer)
-        decoderLayer = Dense(self.hidden_dim, activation='tanh')(encoderLayer)
-        if self.use_dropout:
-            decoderLayer = Dropout(self.dropout)(decoderLayer)
-        decoderLayer = Dense(in_out_neurons_number, activation='sigmoid')(decoderLayer)
-        self.model = Model(inputs=inputLayer, outputs=decoderLayer)
-        self.encoder = Model(inputLayer, encoderLayer)
-
-        #inizializza la tensorboard per l'autoencoder (http://localhost:6006/)
-        tensorboard = TensorBoard(log_dir='./logs/autoencoder', histogram_freq=0, write_graph=True, write_images=True)
-
-        #compila l'autoencoder
-        optimizer = Adam(lr=self.lr)
-        self.model.compile(optimizer=optimizer, loss='mean_squared_error', metrics=['accuracy'])
-
-        #addestra l'autoencoder
-        self.history = self.model.fit(X, X, epochs=self.epochs, batch_size=self.batch_size, shuffle=self.shuffle, validation_split=self.validation_split, verbose=self.verbose, callbacks=[tensorboard])
-        
-        #torna il punteggio del training
-        score = self.model.evaluate(X, X, verbose=0)
-        return score
-
-    def predict(self, X):
-        return self.encoder.predict(X)
-
-    def save(self):
-        self.encoder.save(self.autoencoderModelFile)
-        self.encoder.save_weights(self.autoencoderWeightFile)
-
-    def load(self):
-        self.encoder = load_model(self.autoencoderModelFile)
-        self.encoder.load_weights(self.autoencoderWeightFile)
-
-    def plotTrain(self):
-        # summarize history for accuracy
-        plt.plot(self.history.history['acc'])
-        plt.plot(self.history.history['val_acc'])
-        plt.title('model accuracy')
-        plt.ylabel('accuracy')
-        plt.xlabel('epoch')
-        plt.legend(['train', 'test'], loc='upper left')
-        plt.show()
-        # summarize history for loss
-        plt.plot(self.history.history['loss'])
-        plt.plot(self.history.history['val_loss'])
-        plt.title('model loss')
-        plt.ylabel('loss')
-        plt.xlabel('epoch')
-        plt.legend(['train', 'test'], loc='upper left')
-        plt.show()
-
-
-ae = ann2.Autoencoder()
-ae.verbose=0
-
-scoreAE = ae.buildAndFit(X)
-X = ae.predict(X)
-scoreANN = fc.buildAndFit(X, Y)
-ae.plotTrain()
-print('AE test score:', scoreAE[0])
-print('AE test accuracy:', scoreAE[1])
+path = './data'
+for filename in sorted(os.listdir(path)):
+    print("File:" + filename)
+    df = pd.read_csv(path+"/"+filename, sep='\t')
+    data = df.iloc[:, :].values.astype(np.float32)
+    #data = shuffle(data)
+    data_train, data_test, _, _ = train_test_split(data, data, test_size=0.3, random_state=42)
+    history = model.fit_generator(generator(data_train, batch_size), 
+        epochs=epochs, verbose=1, shuffle=True, steps_per_epoch=data.shape[0]/batch_size,
+        validation_data=generator(data_test, batch_size),validation_steps=data.shape[0]/batch_size)
+    print(history.history)
