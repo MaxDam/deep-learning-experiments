a19ba5f2bf913c826f9498dc66e663580340b23f
Massimiliano D'Amico <max@MacBook-Pro-di-Massimiliano.local>
2021-02-21 17:16:21 +0100

    rstp_mqtt

--- python_mqtt/rtsp_and_mqtt/rtsp_and_mqtt_transformer_haar.py
+++ python_mqtt/rtsp_and_mqtt/rtsp_and_mqtt_transformer_haar.py
@@ -12,11 +12,9 @@ cap = cv.VideoCapture(RTSP_STREAM)
 mqtt_client = mqtt.Client()
 mqtt_client.connect(MQTT_BROKER, 1883)
 
-#faceCascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
-net = cv.dnn.readNetFromCaffe("weights-prototxt.txt", "res_ssd_300Dim.caffeModel")
+faceCascade = cv.CascadeClassifier('model/haarcascade_frontalface_default.xml')
 acquire_in_progress = False
 
-'''
 def detect_and_send_face(acquiredFrame):
 	global acquire_in_progress
 	gray = cv.cvtColor(acquiredFrame, cv.COLOR_BGR2GRAY)
@@ -25,26 +23,6 @@ def detect_and_send_face(acquiredFrame):
 		roi_color = acquiredFrame[y:y+h, x:x+w] 
 		send_image(roi_color)
 	acquire_in_progress = False
-'''
-
-def detect_and_send_face(acquiredFrame):
-	(height, width) = acquiredFrame.shape[:2]
-	blob = cv.dnn.blobFromImage(cv.resize(acquiredFrame, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
-	net.setInput(blob)
-	detections = net.forward()
-	for i in range(0, detections.shape[2]):
-		confidence = detections[0, 0, i, 2]
-		if confidence < 0.5: continue
-		box = detections[0, 0, i, 3:7] * np.array([width, height, width, height])
-		(x1, y1, x2, y2) = box.astype("int")
-		
-		#text = "{:.2f}%".format(confidence * 100) + " ( " + str(y2-y1) + ", " + str(x2-x1) + " )"
-		#y = y1 - 10 if y1 - 10 > 10 else y1 + 10
-		#cv.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-		#cv.putText(frame, text, (x1, y), cv.LINE_AA, 0.45, (0, 0, 255), 2)
-		
-		roi_color = acquiredFrame[y1:y2, x1:x2] 
-		send_image(roi_color)
 	
 def send_image(faceFrame):
 	_, buffer = cv.imencode('.jpg', faceFrame)
---
+++ python_mqtt/rtsp_and_mqtt/rtsp_and_mqtt_transformer_tflite.py
@@ -0,0 +1,66 @@
+#pip install tensorflow==2.3.0 --user
+#pip install paho-mqtt
+import cv2 as cv
+import paho.mqtt.client as mqtt
+import base64
+import numpy as np
+import tensorflow as tf
+#print(tf.__version__)
+
+RTSP_STREAM = "rtsp://localhost:8554/mystream"
+MQTT_BROKER = "test.mosquitto.org"
+MQTT_TOPIC_SEND = "myhome/mx/cserver/detect"
+	
+DEBUG = True
+if DEBUG:
+	cap = cv.VideoCapture(0)
+else:
+	cap = cv.VideoCapture(RTSP_STREAM)
+mqtt_client = mqtt.Client()
+mqtt_client.connect(MQTT_BROKER, 1883)
+
+interpreter = tf.lite.Interpreter(model_path="model/face_detection_front.tflite")
+interpreter.allocate_tensors()
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+acquire_in_progress = False
+
+def detect_and_send_face(acquiredFrame):
+	#(height, width) = acquiredFrame.shape[:2]
+	acquiredFrame = cv.resize(acquiredFrame, (128, 128))
+	input_data = np.array(acquiredFrame, dtype=np.float32)
+	interpreter.set_tensor(input_details[0]['index'], [input_data])
+	interpreter.invoke()
+	rects = interpreter.get_tensor(output_details[0]['index'])
+	scores = interpreter.get_tensor(output_details[2]['index'])
+	for index, score in enumerate(scores[0]):
+		if score > 0.5:
+			box = rects[0][index]
+			y_min = int(max(1, (box[0] * acquiredFrame.height)))
+			x_min = int(max(1, (box[1] * acquiredFrame.width)))
+			y_max = int(min(acquiredFrame.height, (box[2] * acquiredFrame.height)))
+			x_max = int(min(acquiredFrame.width, (box[3] * acquiredFrame.width)))
+			if DEBUG:
+				cv.rectangle(acquiredFrame, (x_min, y_min), (x_max, y_max), (255, 255, 255), 2)
+				cv.imshow('frame', acquiredFrame)
+			else:
+				roi_color = acquiredFrame[y_min:y_max, x_min:x_max] 
+				send_image(roi_color)
+	
+def send_image(faceFrame):
+	_, buffer = cv.imencode('.jpg', faceFrame)
+	jpg_as_text = base64.b64encode(buffer)
+	mqtt_client.publish(MQTT_TOPIC_SEND, jpg_as_text)
+	
+while(cap.isOpened()):
+	ret, frame = cap.read()
+	
+	if not acquire_in_progress:
+		detect_and_send_face(frame)
+	
+	if cv.waitKey(20) & 0xFF == ord('q'):
+		break
+		
+cap.release()
+cv.destroyAllWindows()
+mqtt_client.disconnect()
